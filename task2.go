7443a758ebf0a4a9ff4fa22c6fca88f07532f8a1
karthissit <karthissit@gmail.com>
2024-01-31 11:13:41 +0530

    Update task2.go
    Updated the functionality

--- task2.go
+++ task2.go
@@ -1,4 +1,87 @@
 package main
 
-func main(){
-  }
+import (
+	"fmt"
+)
+
+type OrganizingTeam struct {
+	TeamMembers    []string
+	PrimaryContact string
+}
+
+type NSPEvent struct {
+	EventDate      string
+	EventName      string
+	PrimaryContact string
+	OrganizingTeam OrganizingTeam
+}
+
+func CreateOrganizingTeam(teamMembers []string, primaryContact string) NSPEvent {
+	event := NSPEvent{
+		OrganizingTeam: OrganizingTeam{
+			TeamMembers:    teamMembers,
+			PrimaryContact: primaryContact,
+		},
+	}
+	return event
+}
+
+func ReadOrganizingTeam(event NSPEvent) {
+	fmt.Println("Organizing Team:")
+	fmt.Println("Team Members:", event.OrganizingTeam.TeamMembers)
+	fmt.Println("Primary Contact:", event.OrganizingTeam.PrimaryContact)
+	fmt.Println()
+}
+
+func UpdateOrganizingTeam(event NSPEvent, teamMembers []string, primaryContact string) NSPEvent {
+	if teamMembers != nil && len(teamMembers) != 0 {
+		event.OrganizingTeam.TeamMembers = append(event.OrganizingTeam.TeamMembers, teamMembers...)
+	}
+	if len(primaryContact) != 0 {
+		event.OrganizingTeam.PrimaryContact = primaryContact
+	}
+	return event
+}
+
+func RemoveTeamMembers(event NSPEvent, membersToRemove []string) NSPEvent {
+	var updatedMembers []string
+
+	for _, member := range event.OrganizingTeam.TeamMembers {
+		shouldRemove := false
+		for _, removeMember := range membersToRemove {
+			if member == removeMember {
+				shouldRemove = true
+				break
+			}
+		}
+		if !shouldRemove {
+			updatedMembers = append(updatedMembers, member)
+		}
+	}
+
+	event.OrganizingTeam.TeamMembers = updatedMembers
+	return event
+}
+
+func DeleteOrganizingTeam(event NSPEvent) NSPEvent {
+	event.OrganizingTeam = OrganizingTeam{}
+	return event
+}
+
+func main() {
+
+	event := CreateOrganizingTeam([]string{"Member1", "Member2"}, "TeamLead")
+	ReadOrganizingTeam(event)
+
+	event = UpdateOrganizingTeam(event, []string{"NewMember1", "NewMember2"}, "NewTeamLead")
+	ReadOrganizingTeam(event)
+
+	event = UpdateOrganizingTeam(event, []string{"NewMember3"}, event.OrganizingTeam.PrimaryContact)
+	ReadOrganizingTeam(event)
+
+	event = RemoveTeamMembers(event, []string{"Member1", "Member3"})
+	ReadOrganizingTeam(event)
+
+	event = DeleteOrganizingTeam(event)
+	ReadOrganizingTeam(event)
+}
